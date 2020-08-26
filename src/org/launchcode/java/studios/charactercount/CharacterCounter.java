e22cda30624dfe606854b4d8570a2992b23a5869
Lisa Gordon <lg.official94@gmail.com>
2020-08-25 21:53:15 -0500

    finished class 2 studio!

---
+++ src/org/launchcode/java/studios/charactercount/CharacterCounter.java
@@ -0,0 +1,47 @@
+package org.launchcode.java.studios.charactercount;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class CharacterCounter {
+
+    public static void main(String[] args) throws FileNotFoundException {
+
+//        Scanner input = new Scanner(System.in);
+//        System.out.println("Enter a string: ");
+//        text = input.nextLine();
+
+        String text = "";
+
+        try {
+            Scanner in = new Scanner(new File("content.txt"));
+            text = in.nextLine();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        text = text.toLowerCase();
+        char[] charactersInString = text.toCharArray();
+        HashMap<Character, Integer> counts = new HashMap<>();
+
+        for(char c : charactersInString) {
+            if (c >= 'a' && c <= 'z') {
+                //seeing c for the first time
+                if(!counts.containsKey(c)) {
+                    counts.put(c, 1);
+                } else { //we have already seen c
+                    int seen = counts.get(c);
+                    counts.put(c, seen + 1);
+                }
+            }
+        }
+
+        for(Map.Entry<Character, Integer> entry : counts.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+
+
+    }
+}
